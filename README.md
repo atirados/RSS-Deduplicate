7257fa41fe42570cc53aa3cded9f87718656404a
Adrian Tirados <a.tirados92@gmail.com>
2016-10-17 16:47:04 -0400

    First prototype working

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ RSS Parser that removes redundancy
 
 ## Requirements
 Python feedparser: `pip install feedparser`
+
+## Usage
+`./deduplicate.py [input_feed_list.tsv] [output_feed_list.tsv]`
--- deduplicate.py
+++ deduplicate.py
@@ -3,52 +3,55 @@
 import sys
 import csv
 import feedparser as parser
-import argparse
 
 """ RSS Parser that removes redundant URLs """
 
 __author__ = "Adrian Tirados"
 __email__ = "atirados@hawk.iit.edu"
 
-"""
-parser = argparse.ArgumentParser(
-        description='This scripts optimizes a list of RSS feeds by removing duplicates')
-parser.add_argument(
-        '-f', '--file', help='TSV file containing the RSS feeds', required=True)
-parser.add_argument(
-        '-e', '--empty', help='How to treat empty RSS feeds. Options: keep/discard. ', required=True)
-    args = parser.parse_args()
+# Input and output files
+in_file = sys.argv[1]
+out_file = sys.argv[2]
 
-feed_file = args.file
-empty = args.empty
-"""
-feed_file = "short_list.tsv"
-
+# Global set of links
 all_links = set()
-optimized_feeds = []
+
 
 def getLinks(url):
-	links = set()
-	feed = parseRSS(url)
-	for entry in feed.entries:
-		if not entry.link in all_links:
-			all_links.add(entry.link)
-			links.add(entry.link)
-	return links
+    """
+    Returns all unique links from a RSS feed url
+    """
+    links = set()
+    feed = parseRSS(url)
+    for entry in feed.entries:
+        entry_link = getattr(entry, 'title', '')
+        if entry_link not in all_links:
+            all_links.add(entry_link)
+            links.add(entry_link)
+    return links
+
 
 def parseRSS(url):
+    """
+    Parses a RSS feed url to obtain all its parameters
+    """
     return parser.parse(url)
 
-with open(feed_file) as tsvfile:
-	tsvreader = csv.reader(tsvfile, delimiter="\t")
-	# skip header
-	next(tsvreader, None)
-	for line in tsvreader:
-		print(line)
-		feed_url = line[1]
-		links = getLinks(feed_url)
-		if links: #not empty set
-			optimized_feeds.append(line)
-		print("Number of Unique Links: "+ str(len(all_links)))
 
-print(len(optimized_feeds))
+with open(in_file) as in_tsv:
+    with open(out_file, "w") as out_tsv:
+        tsvreader = csv.reader(in_tsv, delimiter="\t")
+        # skip header
+        next(tsvreader, None)
+        out_tsv.write("#Feed ID\tURL\n")
+        print("Parsing RSS Feeds...")
+        for line in tsvreader:
+            feed_url = line[1]
+            links = getLinks(feed_url)
+            if links:  # not empty set
+                out_tsv.write(line[0] + "\t" + line[1] + "\n")
+                print("Feed with ID " + line[0] + " not redundant.")
+            else:
+                print("Feed with ID " + line[0] + " redundant. Deduplicated.")
+
+print("... RSS Parsing Completed")
